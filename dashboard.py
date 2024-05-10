20c92f8bb73a260afdd1f4a85639c5b76c9cadc8
Vanderlei Neto <jose.vanderlei@ufba.br>
2024-05-10 16:08:10 -0300

    Ajustenado

--- dashboard.py
+++ dashboard.py
@@ -7,31 +7,41 @@ data = {
     "Mês": [
         "Janeiro", "Fevereiro", "Março", "Abril",
         "Maio", "Junho", "Julho", "Agosto",
-        "Setembro", "Outubro", "Novembro", "Dezembro", "Total Anual"
+        "Setembro", "Outubro", "Novembro", "Dezembro"
     ],
-    "EBSCO (R$)": [5000, 1000, 1000, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 8000],
-    "CAPES (R$)": [800, 800, 810, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 3410],
-    "Pergamum (R$)": [2000, 2000, 2500, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6500],
-    "Minha Biblioteca (R$)": [1100, 1100, 1100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3300],
-    "Total Mensal (R$)": [8900, 4900, 5410, 2000, 0, 0, 0, 0, 0, 0, 0, 0, 21210]
+    "EBSCO (R$)": [5000, 1000, 1000, 1000, 0, 0, 0, 0, 0, 0, 0, 0],
+    "CAPES (R$)": [800, 800, 810, 1000, 0, 0, 0, 0, 0, 0, 0, 0],
+    "Pergamum (R$)": [2000, 2000, 2500, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    "Minha Biblioteca (R$)": [1100, 1100, 1100, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    "Total Mensal (R$)": [8900, 4900, 5410, 2000, 0, 0, 0, 0, 0, 0, 0, 0]
 }
 
 df = pd.DataFrame(data)
 
+# Certifique-se de que o DataFrame está na ordem correta dos meses
+meses_ordem_certa = [
+    "Janeiro", "Fevereiro", "Março", "Abril",
+    "Maio", "Junho", "Julho", "Agosto",
+    "Setembro", "Outubro", "Novembro", "Dezembro"
+]
+
+df['Mês'] = pd.Categorical(df['Mês'], categories=meses_ordem_certa, ordered=True)
+df = df.sort_values('Mês')
+
 # Exibir os dados
-st.write("### Gasto da Bibliteca:")
+st.write("### Gastos do SIBI UniFTC:")
 st.write(df)
 
 # Gráficos
-st.write("### Gráficos:")
+st.write("### Fluxo de Gastos:")
 
 # Gráfico de Barras
-st.bar_chart(df.set_index("Mês").drop(columns=["Total Mensal (R$)"]))
+st.bar_chart(df.set_index("Mês"))
 
 # Gráfico de Pizza
-st.write("#### Total por Serviço:")
+st.write("#### Total por Produtos:")
 fig, ax = plt.subplots()
-ax.pie(df.iloc[0:12, 1:5].sum(), labels=df.columns[1:5], autopct='%1.1f%%')
+ax.pie(df.iloc[:, 1:5].sum(), labels=df.columns[1:5], autopct='%1.1f%%')
 st.pyplot(fig)
 
 # Gráfico de Linha
@@ -40,4 +50,5 @@ st.line_chart(df.set_index("Mês")["Total Mensal (R$)"])
 
 # Gráfico de Barras Horizontais Empilhadas para Total Anual por Serviço
 st.write("#### Total Anual por Serviço:")
-st.bar_chart(df.iloc[-1, 1:5])
+total_anual = df.iloc[:, 1:5].sum()
+st.bar_chart(pd.DataFrame(total_anual).T)
